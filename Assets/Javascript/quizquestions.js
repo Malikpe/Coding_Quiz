4a6dbec56a2daf90ae1f17182cadf5001051e156
Malikpe <malikpe@ymail.com>
2023-07-13 12:47:05 -0700

    Intial Commit

---
+++ Assets/Javascript/quizquestions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the shortcut to start a HTML?",
+    answer: "SHIFT + 1",
+    options: [
+      "SHIFT + 1",
+      "ALT + B",
+      "WINDOWS + P",
+      "CTRL + ALT + DEL"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is Javascript?",
+    answer: "Scripting language for webpages",
+    options: [
+      "Cascading style sheet",
+      "Adobe software",
+      "Coffee slang",
+      "Scripting language for webpages"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is NOT a HTML element?",
+    answer: "Src",
+    options: [
+      "Src",
+      "Body",
+      "P",
+      "H1"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When was CSS created?",
+    answer: "1996",
+    options: [
+      "1990",
+      "2000",
+      "1992",
+      "1996"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+];
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Coding_Quiz
+# Chanllenge 4
+
+## User Story
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+### Code Quiz
+
+Take a quiz that have 5 questions that are code related.
